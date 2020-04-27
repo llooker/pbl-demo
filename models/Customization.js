2bad55369bd73902ed75cf6d9a0d32d48a8259db
Elliot Glasenk <eglasenk@gmail.com>
2020-04-27 16:06:52 -0400

    Add customization model with proper capitalization

---
+++ models/Customization.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Customization = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    // customizations: {
+    //     type: Array,
+    //     default: null
+    // }
+    customizations: {
+        type: Object
+    }
+});
+
+module.exports = mongoose.model('Customization', Customization);
