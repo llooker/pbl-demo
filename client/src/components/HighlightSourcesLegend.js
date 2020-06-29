1d9b9daa1a0376e767f850e86c3c187e380a9dd8
Elliot Glasenk <eglasenk@gmail.com>
2020-06-29 13:15:39 -0400

    Review Bryan's code and remove consoles

--- client/src/components/HighlightSourcesLegend.js
+++ client/src/components/HighlightSourcesLegend.js
@@ -3,19 +3,18 @@ import AppContext from '../AppContext';
 import { Box, Typography, Grid } from '@material-ui/core';
 import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
 import { API_COLOR, EMBED_COLOR } from './Highlights/Highlight';
-export function HighlightSourcesLegend( {className}) {
-  
-  const {show} = useContext(AppContext)
-  console.log(className)
+export function HighlightSourcesLegend({ className }) {
+
+  const { show } = useContext(AppContext)
   if (show) {
     return (
-      <Box 
+      <Box
         className={className}
       >
         <Grid container direction="column">
           <Grid item>
-            
-            <Typography variant="subtitle1" ><CheckBoxOutlineBlankIcon style={{ color: API_COLOR }}/> API</Typography>
+
+            <Typography variant="subtitle1" ><CheckBoxOutlineBlankIcon style={{ color: API_COLOR }} /> API</Typography>
           </Grid>
           <Grid>
             <Typography variant="subtitle1" ><CheckBoxOutlineBlankIcon style={{ color: EMBED_COLOR }} /> Embed</Typography>
