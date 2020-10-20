e2833de7ba24f7f64cf8068373608f11bbc461b3
Elliot Glasenk <eglasenk@gmail.com>
2020-10-20 14:12:46 -0400

    Clean up looker and index controllers and mongo related packages

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,13 +1,8 @@
 'use strict'
 
-//const lookerHost = process.env.LOOKER_HOST;
-const lookerHostNameToUse = process.env.LOOKER_HOST.substr(0, process.env.LOOKER_HOST.indexOf('.')) //lookerHost.substr(0, lookerHost.indexOf('.'));
-// const Customization = require('../models/Customization');
-const User = require('../models/User');
-const { makeid } = require('../tools');
+const lookerHostNameToUse = process.env.LOOKER_HOST.substr(0, process.env.LOOKER_HOST.indexOf('.'));
 
-// RG 9/4 adding import for server-side iframe request
-const { createSignedUrl, accessToken } = require('../server_utils/auth_utils')
+const { createSignedUrl, } = require('../server_utils/auth_utils')
 const { Looker40SDK, Looker31SDK, NodeSession, NodeSettingsIniFile } = require('@looker/sdk')
 const settings = new NodeSettingsIniFile()
 const session = new NodeSession(settings)
@@ -18,7 +13,6 @@ module.exports.readSession = async (req, res, next) => {
   // console.log('readSession')
   let { session } = req
   // console.log({ session })
-
   res.status(200).send({ session })
 }
 
@@ -33,28 +27,9 @@ module.exports.writeSession = async (req, res, next) => {
   session.lookerUser.first_name = session.userProfile.givenName;
   session.lookerUser.last_name = session.userProfile.familyName;
 
+  let updatedSession = await tokenHelper(session)
 
-  // Calling the iframe url to ensure the embed user exists
-  const url = await createSignedUrl('/alive', session.lookerUser, process.env.LOOKER_HOST, process.env.LOOKERSDK_EMBED_SECRET);
-  await rp(url)
-
-  // Initialize the API session, sudo and retrieve the bearer token
-  const userCred = await sdk.ok(sdk.user_for_credential('embed', session.userProfile.email));
-  const embeddedUserSession = new NodeSession(settings) // node wrapper
-  // instantiate new sdk client based on embedded session
-  const embeddedUserSdk = new Looker40SDK(embeddedUserSession)
-  // ensure service account connected before sudoing
-  const me = await embeddedUserSdk.ok(embeddedUserSdk.me())
-  const embed_user_token = await embeddedUserSdk.login_user(userCred.id.toString())
-  const u = {
-    // looker_user_id: userCred.id.toString(), //unncessary
-    // google_id: session.userProfile.email, //unncessary
-    api_user_token: embed_user_token.value,
-    api_token_last_refreshed: Date.now()
-  }
-  session.lookerApiToken = { ...u }
-
-  res.status(200).send({ session });
+  res.status(200).send({ session: updatedSession });
 }
 
 module.exports.endSession = async (req, res, next) => {
@@ -66,10 +41,17 @@ module.exports.refreshLookerToken = async (req, res, next) => {
   // console.log('refreshLookerToken')
   let { session } = req;
   // console.log({ session })
+
+  let updatedSession = await tokenHelper(session)
+
+  res.status(200).send({ session: updatedSession });
+}
+
+
+async function tokenHelper(session) {
   // Calling the iframe url to ensure the embed user exists
   const url = await createSignedUrl('/alive', session.lookerUser, process.env.LOOKER_HOST, process.env.LOOKERSDK_EMBED_SECRET);
   await rp(url)
-
   // Initialize the API session, sudo and retrieve the bearer token
   const userCred = await sdk.ok(sdk.user_for_credential('embed', session.userProfile.email));
   const embeddedUserSession = new NodeSession(settings) // node wrapper
@@ -85,7 +67,7 @@ module.exports.refreshLookerToken = async (req, res, next) => {
     api_token_last_refreshed: Date.now()
   }
   session.lookerApiToken = { ...u }
+  return session;
 
-  res.status(200).send({ session });
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,19 @@
 'use strict'
 
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
-const indexCtrl = require('../controllers/indexController')
-// const customizeCtrl = require('../controllers/customizeController')
-const lookerCtrl = require('../controllers/lookerController')
+const indexCtrl = require('../controllers/indexController');
+const lookerCtrl = require('../controllers/lookerController');
 
-//session 
-router.get('/readsession', indexCtrl.readSession)
-router.post('/writesession', indexCtrl.writeSession)
-router.post('/endsession', indexCtrl.endSession)
-router.get('/refreshlookertoken', indexCtrl.refreshLookerToken)
+//session mgmt
+router.get('/readsession', indexCtrl.readSession);
+router.post('/writesession', indexCtrl.writeSession);
+router.post('/endsession', indexCtrl.endSession);
+router.get('/refreshlookertoken', indexCtrl.refreshLookerToken);
 
-//customization
-// router.get('/customize', customizeCtrl.main)
-// router.post('/savecustomization', customizeCtrl.saveCustomization)
-// router.post('/savelookercontent', customizeCtrl.saveLookerContent)
-// router.post('/applyactivecustomziation', customizeCtrl.applyActiveCustomizationToSession)
+//looker mgmt
+router.get('/auth', lookerCtrl.auth);
+router.post('/updatelookeruser', lookerCtrl.updateLookerUser);
 
-//looker
-router.get('/auth', lookerCtrl.auth)
-router.post('/updatelookeruser', lookerCtrl.updateLookerUser) //necessary for persisting user change
-// router.post('/refreshlookertoken', lookerCtrl.refreshLookerToken) //necessary for persisting user change
-
-// router.get('/validatelookercontent/:content_id/:content_type', lookerCtrl.validateLookerContent)
-// router.get('/fetchfolder/:folder_id', lookerCtrl.fetchFolder)
-// router.get('/runquery/:query_id/:result_format', lookerCtrl.runQuery)
-// router.get('/runinlinequery/:inline_query/:result_format', lookerCtrl.runInlineQuery)
-// // router.get('/createquery/:query_body/:result_format', lookerCtrl.createQuery)
-// router.get('/createquerytask/:query_body', lookerCtrl.createQueryTask)
-// router.get('/checkquerytask/:task_id', lookerCtrl.checkQueryTask)
-// router.get('/deletelook/:look_id', lookerCtrl.deleteLook)
-// router.get('/getlook/:look_id', lookerCtrl.getLook);
-// router.get('/getthumbnail/:type/:id', lookerCtrl.getThumbnail);
-// router.get('/validateaccesstoken', lookerCtrl.validateAccessToken);
-
-
-
-
-module.exports = router
+module.exports = router;
