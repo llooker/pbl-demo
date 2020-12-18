668380849d7557bdf2c0d289f1d862ef9245fcc2
Elliot Glasenk <eglasenk@google.com>
2020-12-18 15:09:35 -0800

    Update saved sql commands

--- table.sql
+++ table.sql
@@ -1,20 +1,30 @@
-CREATE TABLE "session" (
-  "sid" varchar NOT NULL COLLATE "default",
+CREATE TABLE "session"
+(
+  "sid" varchar NOT NULL
+  COLLATE "default",
 	"sess" json NOT NULL,
-	"expire" timestamp(6) NOT NULL
+	"expire" timestamp
+  (6) NOT NULL
 )
-WITH (OIDS=FALSE);
+  WITH
+  (OIDS=FALSE);
 
-ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+  ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid")
+  NOT DEFERRABLE INITIALLY IMMEDIATE;
 
-CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+  CREATE INDEX "IDX_session_expire" ON "session" ("expire");
 
-CREATE USER root WITH password 'root';
+  CREATE USER root
+  WITH password 'root';
 
-/*/Applications/Postgres.app/Contents/Versions/12/bin/psql -U root -d atom_fashion_dev*/
+  GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO root;
+  GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO root;
 
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO root;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO root;
+  ALTER USER root WITH SUPERUSER 
 
-ALTER USER root WITH SUPERUSER 
+  -- for connecting to postgres
+  -- local
+  --/Applications/Postgres.app/Contents/Versions/12/bin/psql -U root -d atom_fashion_dev
+  --prod -- don't forget to whitelist current ip address
+  --/Applications/Postgres.app/Contents/Versions/12/bin/psql -U postgres -d atom_fashion -h 34.84.65.32
 
