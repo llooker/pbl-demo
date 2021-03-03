7a6dade34e948081130999215b96b97cff79c9b4
Elliot Glasenk <eglasenk@google.com>
2021-03-03 22:47:24 +0000

    Make repo work for cloud top!

--- table.sql
+++ table.sql
@@ -1,3 +1,18 @@
+
+-- for connecting to postgres
+-- local
+--/Applications/Postgres.app/Contents/Versions/12/bin/psql -U root -d atom_fashion_dev
+--prod -- don't forget to whitelist current ip address
+--/Applications/Postgres.app/Contents/Versions/12/bin/psql -U postgres -d atom_fashion -h 34.84.65.32
+
+CREATE USER root
+WITH password 'root';
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO root;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO root;
+
+ALTER USER root WITH SUPERUSER 
+
 CREATE TABLE "session"
 (
   "sid" varchar NOT NULL
@@ -14,17 +29,4 @@ CREATE TABLE "session"
 
   CREATE INDEX "IDX_session_expire" ON "session" ("expire");
 
-  CREATE USER root
-  WITH password 'root';
-
-  GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO root;
-  GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO root;
-
-  ALTER USER root WITH SUPERUSER 
-
-  -- for connecting to postgres
-  -- local
-  --/Applications/Postgres.app/Contents/Versions/12/bin/psql -U root -d atom_fashion_dev
-  --prod -- don't forget to whitelist current ip address
-  --/Applications/Postgres.app/Contents/Versions/12/bin/psql -U postgres -d atom_fashion -h 34.84.65.32
 
