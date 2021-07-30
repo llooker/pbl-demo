acff03e37cf46a376e1f753e256980714af9b9b9
Elliot Glasenk <eglasenk@google.com>
2021-07-30 12:13:17 +0000

    Add stale commits

--- client/shared/utils/codeSandboxes.js
+++ client/shared/utils/codeSandboxes.js
@@ -1,24 +1,28 @@
 export const codeSandboxes = {
   embedded_dashboard: {
-    link: "https://codesandbox.io/embed/stupefied-smoke-7kyzt?fontsize=14&hidenavigation=1&initialpath=%2Fembedded_dashboard&module=%2Fsrc%2Fclient%2FEmbeddedDashboard.js&theme=light&view=editor",
+    link: "https://codesandbox.io/embed/ecstatic-volhard-87zof?fontsize=14&hidenavigation=1&initialpath=%2Fembedded_dashboard&module=%2Fsrc%2Fclient%2FEmbeddedDashboard.js&theme=light&view=editor",
     label: "Embedded Dashboard"
   },
   embedded_explore:
   {
-    link: "https://codesandbox.io/embed/stupefied-smoke-7kyzt?fontsize=14&hidenavigation=1&initialpath=%2Fembedded_explore&module=%2Fsrc%2Fclient%2FEmbeddedExplore.js&theme=light&view=editor",
+    link: "https://codesandbox.io/embed/ecstatic-volhard-87zof?fontsize=14&hidenavigation=1&initialpath=%2Fembedded_explore&module=%2Fsrc%2Fclient%2FEmbeddedExplore.js&theme=light&view=editor",
     label: "Embedded Explore"
   },
   embedded_query: {
-    link: "https://codesandbox.io/embed/stupefied-smoke-7kyzt?fontsize=14&hidenavigation=1&initialpath=%2Fembedded_query&module=%2Fsrc%2Fclient%2FEmbeddedQuery.js&theme=light&view=editor",
+    link: "https://codesandbox.io/embed/ecstatic-volhard-87zof?fontsize=14&hidenavigation=1&initialpath=%2Fembedded_query&module=%2Fsrc%2Fclient%2FEmbeddedQuery.js&theme=light&view=editor",
     label: "Embedded Query"
   },
   api_run_query:
   {
-    link: "https://codesandbox.io/embed/stupefied-smoke-7kyzt?fontsize=14&hidenavigation=1&initialpath=%2Flooker_api_run_query&module=%2Fsrc%2Fclient%2FApiRunQuery.js&theme=light&view=editor",
+    link: "https://codesandbox.io/embed/ecstatic-volhard-87zof?fontsize=14&hidenavigation=1&initialpath=%2Flooker_api_run_query&module=%2Fsrc%2Fclient%2FApiRunQuery.js&theme=light&view=editor",
     label: "Api Run Query "
   },
   embedded_look: {
-    link: "https://codesandbox.io/embed/stupefied-smoke-7kyzt?fontsize=14&hidenavigation=1&initialpath=%2Fembedded_look&module=%2Fsrc%2Fclient%2FEmbeddedLook.js&theme=light&view=editor",
+    link: "https://codesandbox.io/embed/ecstatic-volhard-87zof?fontsize=14&hidenavigation=1&initialpath=%2Fembedded_look&module=%2Fsrc%2Fclient%2FEmbeddedLook.js&theme=light&view=editor",
+    label: "Embedded Look"
+  },
+  component_sparkline: {
+    link: "https://codesandbox.io/embed/ecstatic-volhard-87zof?fontsize=14&hidenavigation=1&initialpath=%2Fcomponent_sparkline&module=%2Fsrc%2Fclient%2FComponentSparkline.js&theme=light&view=editor",
     label: "Embedded Look"
   },
 }
