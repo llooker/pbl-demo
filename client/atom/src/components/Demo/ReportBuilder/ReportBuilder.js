d012cf99d0a23be4c349782817d393c05ad194a9
danchapman <chappyd23@gmail.com`>
2021-10-06 21:02:35 -0700

    cleanup

--- client/atom/src/components/Demo/ReportBuilder/ReportBuilder.js
+++ client/atom/src/components/Demo/ReportBuilder/ReportBuilder.js
@@ -19,59 +19,49 @@ export default function ReportBuilder(props) {
 
   const dynamicTopBarBottomBarHeight = process.env.REACT_APP_PACKAGE_NAME === "vision" ? 0 : topBarBottomBarHeight;
   const [iFrameExists, setIFrame] = useState(0);
+  const [showLoader, setShowLoader] = useState(0);
   const [apiContent, setApiContent] = useState([]);
   const [exploreObj, setExploreObj] = useState({});
   const [height, setHeight] = useState((window.innerHeight - dynamicTopBarBottomBarHeight));
   const [expansionPanelHeight, setExpansionPanelHeight] = useState(0);
+  const [exploreMode, setExploreMode] = useState(0);
 
   const classes = useStyles();
-  const [value, setValue] = useState(0);
+ 
   const [qid, setQid] = useState(null);
   const { staticContent, staticContent: { lookerContent }, staticContent: { type } } = props;
 
   const demoComponentType = type;
   const tabContent = [...lookerContent]
 
-  const handleChange = (event, newValue) => {
-    if (newValue == 1 && lookerUser.user_attributes.permission_level !== 'premium') {
-      // handleChange(0)
+  const handleChange = (event, newMode) => {
+    if (newMode == 1 && lookerUser.user_attributes.permission_level !== 'premium') {
       setPaywallModal({
         'show': true,
         'permissionNeeded': 'explore'
       });
     } else {
-      setValue(newValue)
+      setExploreMode(newMode)
     }
   };
 
   useEffect(() => {
     if (isReady) {
-      corsApiCall(performLookerApiCalls, [lookerContent, !value])
+      const newMode = !exploreMode;
+      corsApiCall(performLookerApiCalls, [lookerContent, newMode])
     }
-  }, [lookerUser, value, isReady, value]);
+  }, [lookerUser, exploreMode, isReady]);
 
   useEffect(() => {
     window.addEventListener("resize", () => setHeight((window.innerHeight - dynamicTopBarBottomBarHeight)));
     setExpansionPanelHeight($('.MuiExpansionPanel-root:visible').innerHeight() || 0)
   })
 
-
   const action = async (contentType, contentId, secondaryAction, qid, exploreId, newReportEmbedContainer) => {
     // console.log("action");
     // console.log({ contentType })
     // console.log({ contentId })
     // console.log({ qid })
-    let iFrameArray = $(".embedContainer:visible > iframe");
-
-    let matchingIndex = 0;
-    for (let i = 0; i < iFrameArray.length; i++) {
-      if (iFrameArray[i].classList.contains(contentType) && iFrameArray[i].classList.contains(contentId)) {
-        iFrameArray[i].classList.remove('dNone')
-        matchingIndex = i;
-      } else {
-        iFrameArray[i].classList.add('dNone')
-      }
-    }
 
     if (secondaryAction === 'edit' || secondaryAction === 'explore') {
       //save qid to state and use in perform looker api calls
@@ -81,15 +71,26 @@ export default function ReportBuilder(props) {
 
       let lookerResponse = await sdk.ok(sdk.delete_look(contentId));
       corsApiCall(performLookerApiCalls, [lookerContent, 1])
+    } else {
+      // default is to load a specific piece of content
+      corsApiCall(performLookerApiCalls, [lookerContent, 0, contentId, contentType])
     }
   }
 
-  const performLookerApiCalls = function (lookerContent, animateLoad) {
+  const performLookerApiCalls = function (lookerContent, animateLoad, contentId, contentType) {
     // console.log("performLookerApiCalls")
     // console.log({ lookerContent })
     // console.log({ animateLoad })
 
+    // clear iFrame
     $(`.embedContainer.${validIdHelper(demoComponentType)}:visible`).html('')
+    setShowLoader(1)
+
+    // set default report object
+    const contentMetadata = {
+      type: contentType ? contentType : "report", 
+      id: contentId ? contentId : null,
+    }
 
     if (animateLoad) {
       handleChange('refresh', 0)
@@ -97,17 +98,24 @@ export default function ReportBuilder(props) {
       setApiContent([])
     }
 
+    // if it's a collection of save reports
     lookerContent.map(async lookerContent => {
-      if (value === 0 &&
+      if (exploreMode === 0 &&
         lookerContent.type === 'folder') {
 
         const sharedFolder = await sdk.ok(sdk.folder(lookerContent.id));
         const embedUser = await sdk.ok(sdk.me())
 
         for (let h = 0; h < sharedFolder.looks.length; h++) {
-          let look = await sdk.ok(sdk.look(sharedFolder.looks[h].id))
-          let clientId = look.query.client_id;
-          sharedFolder.looks[h].client_id = clientId;
+          // set to a default look if there isn't one
+          if (!contentMetadata.id) contentMetadata.id = sharedFolder.looks[h].id
+              
+          // only lookup the selected look
+          if(contentMetadata.id === sharedFolder.looks[h].id) {
+            let look = await sdk.ok(sdk.look(sharedFolder.looks[h].id))
+            let clientId = look.query.client_id;
+            sharedFolder.looks[h].client_id = clientId;
+          }
         }
 
         let embeddedUserFolder = {}
@@ -117,9 +125,15 @@ export default function ReportBuilder(props) {
             let embedUserFolderId = embedUserLooks[0].folder_id || null;
             embeddedUserFolder = await sdk.ok(sdk.folder(embedUserFolderId));
             for (let i = 0; i < embeddedUserFolder.looks.length; i++) {
-              let look = await sdk.ok(sdk.look(embeddedUserFolder.looks[i].id));
-              let clientId = look.query.client_id;
-              embeddedUserFolder.looks[i].client_id = clientId;
+              // set a default look if there isn't one
+              if (!contentMetadata.id) contentMetadata.id = embeddedUserFolder.looks[i].id
+              
+              // only lookup the selected look
+              if(contentMetadata.id === embeddedUserFolder.looks[i].id) {
+                let look = await sdk.ok(sdk.look(embeddedUserFolder.looks[i].id));
+                let clientId = look.query.client_id;
+                embeddedUserFolder.looks[i].client_id = clientId;
+              }
             }
           }
         }
@@ -135,47 +149,36 @@ export default function ReportBuilder(props) {
           looksToUse = [
             ...looksToUse, ...lookerResponseData.embeddedUserFolder.looks
           ]
-
         }
         let dashboardsToUse = [...lookerResponseData.sharedFolder.dashboards]
         let objToUse = {
           reports: looksToUse,
           dashboards: dashboardsToUse
         }
-        let iFrameArray = $(`.embedContainer.${validIdHelper(demoComponentType)} > iframe`);
+
         if (objToUse.reports.length) {
           objToUse.reports.map((item, index) => {
-
             let lookId = item.id;
-            let lookIsRendered = false;
-            for (let i = 0; i < iFrameArray.length; i++) {
-              if (iFrameArray[i].classList.contains('look') && iFrameArray[i].classList.contains(lookId)) {
-                lookIsRendered = true;
-              }
-            }
 
-            if (!lookIsRendered) {
+            if (lookId === contentMetadata.id) {
               LookerEmbedSDK.createLookWithId(lookId)
                 .appendTo(validIdHelper(`#embedContainer-${demoComponentType}-${lookerContent.id}`))
                 .withClassName('iframe')
                 .withClassName('report')
                 .withClassName('look')
                 .withClassName(lookerResponseData.sharedFolder.looks.indexOf(item) > -1 ? "shared" : "personal")
-                .withClassName(index > 0 ? 'dNone' : 'oops')
                 .withClassName(lookId)
                 .build()
                 .connect()
                 .then((look) => {
                   let modifiedBaseUrl = clientSession.lookerBaseUrl.substring(0, clientSession.lookerBaseUrl.lastIndexOf(":"));
                   LookerEmbedSDK.init(modifiedBaseUrl)
+                  setShowLoader(0)
+                  setIFrame(1)
                 })
                 .catch((error) => {
                   console.error('Connection error', error)
                 })
-            }
-
-            if (index === objToUse.reports.length - 1) {
-              setTimeout(() => setIFrame(1), 1000)
             }
           })
         }
@@ -183,26 +186,19 @@ export default function ReportBuilder(props) {
         if (objToUse.dashboards.length) {
           objToUse.dashboards.map((item, index) => {
             let dashboardId = item.id
-            let dashboardIsRendered = false;
-            for (let i = 0; i < iFrameArray.length; i++) {
-              if (iFrameArray[i].classList.contains('dashboard') && iFrameArray[i].classList.contains(dashboardId)) {
-                dashboardIsRendered = true;
-              }
-            }
-            if (!dashboardIsRendered) {
+
+            if (dashboardId === contentMetadata.id) {
               LookerEmbedSDK.createDashboardWithId(dashboardId)
                 .appendTo(validIdHelper(`#embedContainer-${demoComponentType}-${lookerContent.id}`))
                 .withClassName('iframe')
                 .withClassName('dashboard')
                 .withClassName(lookerResponseData.sharedFolder.dashboards.indexOf(item) > -1 ? "shared" : "personal")
-                .withClassName('dNone')
                 .withClassName(dashboardId)
                 .build()
                 .connect()
                 .then((dashboard) => {
-                  setTimeout(() => {
-                    // setIFrame(1)
-                  }, 1000)
+                  setShowLoader(0)
+                  setIFrame(1)
                   let modifiedBaseUrl = clientSession.lookerBaseUrl.substring(0, clientSession.lookerBaseUrl.lastIndexOf(":"));
                   LookerEmbedSDK.init(modifiedBaseUrl)
 
@@ -211,16 +207,15 @@ export default function ReportBuilder(props) {
                   console.error('Connection error', error)
                 })
             }
-
-            if (index === objToUse.dashboards.length - 1) {
-              setTimeout(() => setIFrame(1), 1000)
-            }
           })
         }
         setApiContent(objToUse)
       } else if (lookerContent.type === 'explore' &&
+
+        // or it's an explore
         lookerUser.user_attributes.permission_level === 'premium' &&
-        value === 1) {
+        exploreMode === 1) {
+        
         let exploreId = lookerContent.id;
         $(validIdHelper(`#embedContainer-${demoComponentType}-${lookerContent.id}`)).html('');
         //separate logic for embedding explore with qid vs. no qid
@@ -236,6 +231,7 @@ export default function ReportBuilder(props) {
             .build()
             .connect()
             .then((explore) => {
+              setShowLoader(1)
               setIFrame(1)
               setExploreObj(explore)
 
@@ -256,6 +252,7 @@ export default function ReportBuilder(props) {
             .build()
             .connect()
             .then((explore) => {
+              setShowLoader(0)
               setIFrame(1)
               setExploreObj(explore)
               let modifiedBaseUrl = clientSession.lookerBaseUrl.substring(0, clientSession.lookerBaseUrl.lastIndexOf(":"));
@@ -282,12 +279,11 @@ export default function ReportBuilder(props) {
               classes={classes}
               height={height - expansionPanelHeight}
             />
-            {/* additional loading logic, need embedContainer to exist but want it hidden until iFrame has content...*/}
-            <Box className={iFrameExists ? `` : `${classes.hidden}`}>
+            <Box>
               <AppBar position="static" elevation={0}>
                 <Tabs
                   className={classes.tabs}
-                  value={value}
+                  value={exploreMode}
                   onChange={handleChange}
                   aria-label="simple tabs example"
                 // TabIndicatorProps={{ style: { border: "white" } }}
@@ -327,7 +323,7 @@ export default function ReportBuilder(props) {
                   {tabContent.map((tabContentItem, tabContentItemIndex) => (
                     <TabPanel
                       key={`${validIdHelper(demoComponentType + '-tabPanel-' + tabContentItemIndex)}`}
-                      value={value}
+                      value={exploreMode}
                       index={tabContentItemIndex}
                       style={{ width: '100%' }} //weird
                     >
@@ -353,6 +349,11 @@ export default function ReportBuilder(props) {
                                 </ApiHighlight>
                               </Grid>
                               <Grid item sm={8} >
+                                <Loader
+                                  hide={!showLoader}
+                                  classes={classes}
+                                  height={height - expansionPanelHeight}
+                                />
                                 <EmbedHighlight classes={classes}>
                                   <div
                                     className={`embedContainer ${validIdHelper(demoComponentType)}`}
@@ -365,6 +366,11 @@ export default function ReportBuilder(props) {
                             </React.Fragment>
                             :
                             <Grid item sm={12} >
+                              <Loader
+                                hide={!showLoader}
+                                classes={classes}
+                                height={height - expansionPanelHeight}
+                              />
                               <EmbedHighlight classes={classes}>
                                 <div
                                   className="embedContainer"
