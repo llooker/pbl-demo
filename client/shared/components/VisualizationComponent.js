9212e9e9b06d28cf02903dae80119569233cb3d2
Elliot Glasenk <eglasenk@google.com>
2022-06-09 20:15:05 +0000

    Update packages 2x and wrap visualization component in components provider

--- client/shared/components/VisualizationComponent.js
+++ client/shared/components/VisualizationComponent.js
@@ -1,16 +1,27 @@
 import React, {   useContext } from 'react';
 import { appContextMap} from '../utils/tools';
 import { Query, Visualization,  } from "@looker/visualizations";
+import { ComponentsProvider } from "@looker/components";
+import { DataProvider } from "@looker/components-data";
+import { i18nInit as i18nInitVis, i18nResources } from "@looker/visualizations";
+
+i18nInitVis();
 
 export function VisualizationComponent({ item }) {
   // console.log("VisualizationComponent")
   // console.log({item})
   const {  sdk } = useContext(appContextMap[process.env.REACT_APP_PACKAGE_NAME]);
   return (
+    <DataProvider sdk={sdk}>
+    <ComponentsProvider loadGoogleFonts resources={i18nResources}>
+    
     <Query sdk={sdk} query={item.id || item.qid} 
       config={item.config ? item.config : {}}
     >
       <Visualization height={item.height} />
     </Query>
+
+    </ComponentsProvider>
+    </DataProvider>
   )
 };
